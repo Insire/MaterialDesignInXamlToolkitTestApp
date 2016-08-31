6f87d7661549d4d6b7d2bc91a633c6da5ac193e3
Insire Thomson <insirethomson@gmail.com>
2016-08-31 18:30:17 +0200

    testing flyouts

--- TestApp/MainWindow.xaml.cs
+++ TestApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,18 @@ namespace TestApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var flyout = Flyouts.Items[0] as Flyout;
+
+            flyout.IsOpen = !flyout.IsOpen;
         }
     }
 }
