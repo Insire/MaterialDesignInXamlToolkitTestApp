f6f812db46649123f972fb4f409f219233f4b268
Insire <insirethomson@gmail.com>
2016-09-15 19:55:32 +0200

    more tests

---
+++ TestApp/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Model> Items { get; set; }
+
+        public ViewModel()
+        {
+            Items = new ObservableCollection<Model>
+            {
+                new Model {Name="Name1" },
+                new Model {Name="Name2" },
+                new Model {Name="Name3" },
+                new Model {Name="Name4" },
+                new Model {Name="Name5" },
+            };
+        }
+    }
+
+    public class Model
+    {
+        public string Name { get; set; }
+    }
+}
