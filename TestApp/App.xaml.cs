6b74a1153f4c92566f67122d88b8a9bb90f64d5f
Insire <insirethomson@gmail.com>
2017-04-06 18:14:09 +0200

    custom swatches

--- TestApp/App.xaml.cs
+++ TestApp/App.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MaterialDesignColors;
+using MaterialDesignThemes.Wpf;
 using System.Windows;
+using System.Windows.Media;
 
 namespace TestApp
 {
@@ -13,5 +10,47 @@ namespace TestApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var helper = new PaletteHelper();
+
+            var primary = new Swatch(
+                "ITB primary",
+                new[]
+                {
+                    new Hue("a", Colors.White,Colors.Black),
+                    new Hue("a", Colors.White,Colors.Black),
+                    new Hue("a", Colors.White,Colors.Black),
+                },
+                new[]
+                {
+                    new Hue("a", Colors.White,Colors.Black),
+                    new Hue("a", Colors.White,Colors.Black),
+                    new Hue("a", Colors.White,Colors.Black),
+                }
+            );
+
+            var accent = new Swatch(
+                "ITB accent",
+                new[]
+                {
+                    new Hue("a", Colors.White,Colors.Black),
+                    new Hue("a", Colors.White,Colors.Black),
+                    new Hue("a", Colors.White,Colors.Black),
+                },
+                new[]
+                {
+                    new Hue("a", Colors.White,Colors.Black),
+                    new Hue("a", Colors.White,Colors.Black),
+                    new Hue("a", Colors.White,Colors.Black),
+                }
+            );
+
+            var palette = new Palette(primary, accent, 1, 1, 1, 1);
+
+            helper.ReplacePalette(palette);
+
+            base.OnStartup(e);
+        }
     }
 }
